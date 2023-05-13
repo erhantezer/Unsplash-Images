6e1ce22d331f5eb2e65ab69e738e5bdd91fe593d
Erhan <erhantezer68@gmail.com>
2023-05-13 09:13:29 +0300

    url and serch and key added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 
+
+const clientID = `?client_id=-WsG44wZbP_-YsLll3ho70imGuxqUSGtc2kbBajE7Bc`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+
 function App() {
   const [search, setSearch] = useState("cat");
   const [loading, setLoading] = useState(false);
